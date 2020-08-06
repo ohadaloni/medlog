8ed0684c48efdb30400dd57489bd6203c5b6208f
ohad <ohad@theora.com>
2020-08-06 11:00:46 +0300

    gitDiffs

--- src/index.php
+++ src/index.php
@@ -5,6 +5,7 @@ require_once(M_DIR."/mfiles.php");
 require_once("medLogFiles.php");
 require_once("MedLog.class.php");
 /*------------------------------------------------------------*/
+date_default_timezone_set("UTC");
 global $Mview;
 global $Mmodel;
 $Mview = new Mview;
