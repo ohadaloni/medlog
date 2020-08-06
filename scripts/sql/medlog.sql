e68914e16cb7e35a7f698ab22948edbfea5be78c
ohad <ohad@theora.com>
2020-08-06 09:09:51 +0300

    SnapShot

--- scripts/sql/medlog.sql
+++ scripts/sql/medlog.sql
@@ -1,9 +1,8 @@
 create table medLog (
   id int auto_increment,
   user varchar(255),
-  dosage double,
-  mg varchar(255),
   description varchar(255),
+  quantity varchar(255),
   date date,
   datetime datetime,
   comments text,
