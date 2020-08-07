3fdec0f1fb5af5b0b8e3afb613d0c89661c2abf6
ohad <ohad@theora.com>
2020-08-07 11:09:57 +0300

    gitDiffs

--- src/MedLog.class.php
+++ src/MedLog.class.php
@@ -70,6 +70,7 @@ class MedLog extends Mcontroller {
 				$menu->index();
 			}
 			$this->showMsgs();
+			$this->alarms();
 		}
 	}
 	/*------------------------------------------------------------*/
@@ -361,6 +362,64 @@ class MedLog extends Mcontroller {
 		$this->exportToExcel($rows, "MedLog.$loginName.$date");
 	}
 	/*------------------------------------------------------------*/
+	/*------------------------------------------------------------*/
+	private function alarms() {
+		$loginName = $this->loginName;
+		if ( ! $loginName )
+			return;
+		$myCond = "user = '$loginName'";
+		$ago = date("Y-m-d", time() - 31*24*3600);
+		$agoCond = "date > '$ago'";
+		$conds = "$myCond and $agoCond";
+		$fields = "description, max(id) as id";
+		$groupBy = "group by 1";
+		$sql = "select $fields from medLog where $conds $groupBy";
+		$rows = $this->Mmodel->getRows($sql);
+		foreach ( $rows as $row )
+			$this->alarm($row['description']);
+	}
+	/*------------------------------*/
+	private function alarm($description) {
+		$loginName = $this->loginName;
+		$myCond = "user = '$loginName'";
+		$dCond = "description = '$description'";
+		$conds = "$myCond and $dCond";
+		$sql = "select datetime from medLog where $conds order by datetime desc limit 3";
+		$times = $this->Mmodel->getStrings($sql);
+		if ( count($times) != 3 )
+			return;
+		$now = time();
+		$time0 = strtotime($times[2]);
+		$time1 = strtotime($times[1]);
+		$time2 = strtotime($times[0]);
+		$diff1 = $time1 - $time0;
+		$diff2 = $time2 - $time1;
+		$diff2now = $now - $time2;
+
+		// diff1 & diff2 must be resonably the same
+		$d12 = $diff1 / $diff2 ;
+		if ( $d12 < 0.75 || $d12 > 1.25 ) {
+			// for debugging only:
+			/*	$this->Mview->msg("alarm($description): recent history too caotic");	*/
+			return;
+		}
+		$avg = round(( $diff1 + $diff2) / 2) ;
+
+		// discontinued
+		if ( $diff2now > ( $avg * 2 ) )
+			return;
+
+		// not yet
+		if ( $diff2now < ( $avg * 0.8 ) )
+			return;
+		$time2take = $time2 + $avg;
+		$datetime2take = date("G:i", $time2take);
+		if ( $time2take < $now )
+			$msg = "Missed $datetime2take?";
+		else
+			$msg = "Due at $datetime2take";
+		$this->Mview->urlMsg("$description: $msg", "/medlog/history?description=$description");
+	}
 	/*------------------------------------------------------------*/
 	private function add() {
 		$this->Mview->showTpl("medLog/add.tpl");
