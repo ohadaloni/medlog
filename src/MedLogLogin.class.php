b92394e28ea6661644fb296250f3d009f6a9093d
ohad <ohad@theora.com>
2022-05-01 09:19:46 +0300

    gitDiffs

--- src/MedLogLogin.class.php
+++ src/MedLogLogin.class.php
@@ -2,10 +2,6 @@
 /*------------------------------------------------------------*/
 class MedLogLogin extends Mcontroller {
 	/*------------------------------------------------------------*/
-	public function __construct() {
-		parent::__construct();
-	}
-	/*------------------------------------------------------------*/
 	public function enterSession() {
 		if ( isset($_REQUEST['LogOut']) ) {
 			Mlogin::logout();
