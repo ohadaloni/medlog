1745d53294390fe0c85e6d969f3bfd1114b680e9
ohad <ohad@theora.com>
2022-12-02 20:55:47 +0200

    gitDiffs

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,9 @@
+/*------------------------------------------------------------*/
+function clocks() {
+	clock();
+	stopWatch();
+}
+/*------------------------------------------------------------*/
 function clock() {
   const time = new Date();
   Dn = time.getDay();
@@ -13,7 +19,37 @@ function clock() {
   setTimeout(clock, 1000);
 }
 
-function pad0(i) {
-  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-  return i;
+/*------------------------------------------------------------*/
+var startTime;
+/*------------------------------------------------------------*/
+function stopWatch() {
+	const time = new Date();
+	if ( startTime === undefined )
+		startTime = time.getTime();
+
+	now = time.getTime() - startTime;
+	seconds = Math.floor(now / 1000);
+	/*	alert(startTime + ", now: " + now + ", seconds:" + seconds);	*/
+	secs = seconds % 60 ;
+	minutes = ( seconds - secs ) / 60;
+	mins = minutes % 60;
+	hours = ( minutes - mins ) /60;
+	if ( hours > 0 )  {
+		secs = pad0(secs);
+		mins = pad0(mins);
+		str = hours + ":" + mins + ":" + secs ;
+	} else if ( mins > 0 ) {
+		secs = pad0(secs);
+		str = mins + ":" + secs ;
+	} else {
+		str = secs;
+	}
+	document.getElementById('stopWatch').innerHTML = str;
+	setTimeout(this.stopWatch, 1000);
 }
+/*------------------------------------------------------------*/
+function pad0(i) {
+if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+return i;
+}
+/*------------------------------------------------------------*/
