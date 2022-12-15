18247c33d3391d8722fe001ec7a669c4e848490a
ohad <ohad@theora.com>
2022-12-15 23:57:15 +0200

    gitDiffs

--- src/Menu.class.php
+++ src/Menu.class.php
@@ -19,29 +19,28 @@ class Menu extends Mcontroller {
 		$menu = array(
 			'Medlog' => array(
 				array(
-					'name' => 'tz',
 					'title' => $tzTitle,
 					'url' => "/medLog/myTimeZone",
 				),
 				array(
-					'name' => 'export',
 					'title' => 'Export CSV',
 					'url' => "/medLog/export",
 				),
 				array(
-					'name' => 'lately',
 					'title' => 'Lately',
 					'url' => "/medLog/lately",
+				),
+				array(
+					'title' => 'All',
+					'url' => "/medLog?complete=complete",
 				),
 			),
 			'admin' => array(
 				array(
-					'name' => 'showSource',
 					'title' => 'Show Source Code',
 					'url' => "/showSource",
 				),
 				array(
-					'name' => 'clone',
 					'title' => 'Clone',
 					'url' => "https://github.com/ohadaloni/medlog",
 					'target' => "clone",
@@ -49,12 +48,10 @@ class Menu extends Mcontroller {
 			),
 			$loginName => array(
 				array(
-					'name' => 'chpass',
 					'title' => 'Change Password',
 					'url' => "/medLog/changePasswd",
 				),
 				array(
-					'name' => 'logout',
 					'title' => 'Log Off',
 					'url' => "/?logOut=logOut",
 				),
